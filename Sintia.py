ebaa8804d59e76a0763ed92081a5791316805bd0
João Victor Ramalho Pereira <93351925+jvictorrp15@users.noreply.github.com>
2023-05-02 22:01:04 -0300

    Update Sintia.py

--- Sintia.py
+++ Sintia.py
@@ -4,45 +4,46 @@ import datetime
 import wikipedia
 import pywhatkit
 
+# inicialização dos módulos
+audio = sr.Recognizer() # módulo para reconhecimento de voz
+maquina = pyttsx3.init() # módulo para sintetização de voz
 
-audio = sr.Recognizer()
-maquina = pyttsx3.init()
-
+# função para execução de comando de voz
 def executa_comando():
     try:
-        with sr.Microphone() as source:
-            print('Ouvindo..')
-            voz = audio.listen(source)
-            comando = audio.recognize_google(voz, language='pt-BR')
-            comando = comando.lower()
-            if 'Sintia' in comando:
-                comando = comando.replace('Sintia', '')
-                maquina.say(comando)
-                maquina.runAndWait()
+        with sr.Microphone() as source: # utiliza o microfone como entrada
+            print('Ouvindo..') # imprime mensagem no console
+            voz = audio.listen(source) # aguarda o comando de voz
+            comando = audio.recognize_google(voz, language='pt-BR') # reconhece o comando de voz e define a linguagem
+            comando = comando.lower() # converte o comando para minúsculo
+            if 'Sintia' in comando: # se a palavra "Sintia" for detectada no comando
+                comando = comando.replace('Sintia', '') # remove a palavra "Sintia" do comando
+                maquina.say(comando) # sintetiza o comando de voz em voz alta
+                maquina.runAndWait() # espera a finalização da execução da fala
 
     except:
-        print('Microfone não está ok')
+        print('Microfone não está ok') # se houver algum erro, imprime mensagem no console
 
-    return comando
+    return comando # retorna o comando de voz
 
+# função para execução de ações baseadas no comando de voz
 def comando_voz_usuario():
-    comando = executa_comando()
-    if 'horas' in comando:
-        hora = datetime.datetime.now().strftime('%H:%M')
-        maquina.say('Agora são' + hora)
-        maquina.runAndWait()
-    elif 'procure por' in comando:
-        procurar = comando.replace('procure por', '')
-        wikipedia.set_lang('pt')
-        resultado = wikipedia.summary(procurar,2)
-        print(resultado)
-        maquina.say(resultado)
-        maquina.runAndWait()
-    elif 'toque' in comando:
-        musica = comando.replace('toque','')
-        resultado = pywhatkit.playonyt(musica)
-        maquina.say('Tocando música')
-        maquina.runAndWait()
+    comando = executa_comando() # recebe o comando de voz a partir da função "executa_comando"
+    if 'horas' in comando: # se o comando de voz contém a palavra "horas"
+        hora = datetime.datetime.now().strftime('%H:%M') # obtém a hora atual
+        maquina.say('Agora são' + hora) # sintetiza a hora atual em voz alta
+        maquina.runAndWait() # espera a finalização da execução da fala
+    elif 'procure por' in comando: # se o comando de voz contém a expressão "procure por"
+        procurar = comando.replace('procure por', '') # remove a expressão "procure por" do comando
+        wikipedia.set_lang('pt') # define a linguagem da pesquisa no wikipedia como português
+        resultado = wikipedia.summary(procurar,2) # pesquisa no wikipedia o que foi solicitado e define o número de frases a serem retornadas
+        print(resultado) # imprime o resultado no console
+        maquina.say(resultado) # sintetiza o resultado em voz alta
+        maquina.runAndWait() # espera a finalização da execução da fala
+    elif 'toque' in comando: # se o comando de voz contém a palavra "toque"
+        musica = comando.replace('toque','') # remove a palavra "toque" do comando
+        resultado = pywhatkit.playonyt(musica) # pesquisa a música no YouTube e a reproduz
+        maquina.say('Tocando música') # sintetiza mensagem informando que a música está sendo tocada
+        maquina.runAndWait() # espera a finalização da execução da fala
 
-comando_voz_usuario()
-    
+comando_voz_usuario() # chama a função "comando_voz_usuario" para execução do código.
